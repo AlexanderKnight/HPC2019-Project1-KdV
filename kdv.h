64e975895b30e1f2ea27856722bdb965ee39b0fe
fisiceoir <alexanderlknight@protonmail.com>
2019-03-28 11:25:35 -0400

    Add rk4.c and update kdv.h with rk4 function

--- kdv.h
+++ kdv.h
@@ -6,6 +6,14 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
+/* 4th-Order Runge Kutta
+  Takes the current value y_0
+  at time t_0, and steps forward
+  in time by h. 
+  Requires EoS function (func) 
+  to be defined in this file.*/
+double rk4(double y_0, t_0, h);
+
 static inline double Wtime(void)
 {
 	struct timeval tv;
--- rk4.c
+++ rk4.c
@@ -1,1 +1,37 @@
 #include "kdv.h"
+
+double k1(double y_0, double t_0)
+{
+	return func(y_0, t_0);
+}
+
+double k2(double y_0, double t_0, double h)
+{
+	double y = y_0 + k1(y_0,t_0)*(h/2.);
+	double t = t_0 + (h/2.);
+	return func(y,t);
+}
+
+double k3(double y_0, double t_0, double h)
+{
+	double y = y_0 + k2(y_0, t_0, h)*(h/2.);
+	double t = t_0 + (h/2.);
+	return func(y,t);
+}
+
+double k4(double y_0, double t_0, double h)
+{
+	double y = y_0 + k3(y_0,t_0,h)*h;
+	double t = t_0 + h;
+	return func(y,t);
+}
+
+double rk4(double y_0, double t_0, double h)
+{
+	double m = (1./6.)*k1(y_0,t_0)+
+			(1./3.)*k2(y_0,t_0,h)+
+			(1./3.)*k3(y_0,t_0,h)+
+			(1./6.)*k4(y_0,t_0,h);
+	return y_0 + m*h;
+}
+
