eb003228c0e940d9b6cbcbe31a3b942fea58cff9
Alexander Knight <Alexanderlknight@protonmail.com>
2019-04-08 19:46:06 -0400

    fixed possible neg sign error

--- func.cpp
+++ func.cpp
@@ -5,7 +5,7 @@ double func(double y_bb,double y_b,
 		double y_a,double y_aa,
 		double deltaX)
 {
-	double a = (y_aa -2.*y_a+2.*y_b-y_bb)/(2.*deltaX*deltaX*deltaX);
+	double a = -1.*(y_aa - (2.*y_a) + (2.*y_b)- y_bb)/(2.*deltaX*deltaX*deltaX);
 	double b = (3.*y_0*(y_a-y_b))/(deltaX);
 	return a-b;
 }
