a3d5907e407146975ab7aac22cc5cf52ea1cfdfd
Jeff Lynn <jhl1002@wildcats.unh.edu>
2019-04-10 22:44:43 -0400

    added tracker for first iter

--- rk4.cpp
+++ rk4.cpp
@@ -2,15 +2,25 @@
 #include <iostream>
 
 
-double* k(double *y, double *ki, int size, double deltaX, double h)
+double* k(double *y, double *ki, int size, double deltaX, double h, int first)
 {
 	double *y_temp = new double[size];
 	for (int i=0;i<size;i++){
 		double y_i;
 		int *near = new int[5];
 		near=checkBounds(i,size);
-		y_i = y[i]+ki[i]*h;
-		y_temp[i]=func(ki[near[0]],ki[near[1]],y_i,ki[near[3]],ki[near[4]],deltaX);
+
+		if( first == 0)
+		{
+			y_i = y[i]+ki[i]*h;
+			y_temp[i]=func(y[near[0]],y[near[1]],y_i,y[near[3]],y[near[4]],deltaX);
+		}
+		else
+		{
+			y_i = y[i]+ki[i]*h;
+			y_temp[i]=func(ki[near[0]],ki[near[1]],y_i,ki[near[3]],ki[near[4]],deltaX);
+		}
+		
 		printf("%g, ",y_temp[i]);
 	}
 	return y_temp;
@@ -24,13 +34,13 @@ double* rk4(double* y, int size, double deltaX, double h)
 	double *k3 = new double[size];
 	double *k4 = new double[size];
 	//k1 = k(y, y_temp, size, deltaX, 0.);
-	k1 = k(y, y, size, deltaX, 0.);
+	k1 = k(y, y, size, deltaX, 0., 0);
 	printf("done with k1\n");
-	k2 = k(y, k1, size, deltaX, h/2.);
+	k2 = k(y, k1, size, deltaX, h/2., 1);
 	printf("done with k2\n");
-	k3 = k(y, k2, size, deltaX, h/2.);
+	k3 = k(y, k2, size, deltaX, h/2.,1);
 	printf("done with k3\n");
-	k4 = k(y, k3, size, deltaX, h);
+	k4 = k(y, k3, size, deltaX, h,1);
 	printf("done with k4\n");
 	
 	double y_new[size] = {0};
