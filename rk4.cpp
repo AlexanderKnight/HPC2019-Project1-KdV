f24017f4041905c4738f687c087d849fe9abd6dc
Alexander Knight <Alexanderlknight@protonmail.com>
2019-04-10 22:17:33 -0400

    I'm an idiot

--- rk4.cpp
+++ rk4.cpp
@@ -18,13 +18,16 @@ double* k(double *y, double *ki, int size, double deltaX, double h)
 double* rk4(double* y, int size, double deltaX, double h)
 {
 	double y_new[size];
-	double y_temp[size];
 	double *k1 = new double[size];
 	double *k2 = new double[size];
 	double *k3 = new double[size];
 	double *k4 = new double[size];
-	k1 = k(y, y_temp, size, deltaX, 0.);
+	k1 = k(y, y, size, deltaX, 0.);
 	printf("done with k1\n");
+	for (int i=0;i<size;i++){
+		printf("%g, ",k1[i]);
+	}
+	printf("\n");
 	k2 = k(y, k1, size, deltaX, h/2.);
 	printf("done with k2\n");
 	k3 = k(y, k2, size, deltaX, h/2.);
